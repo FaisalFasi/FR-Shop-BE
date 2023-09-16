eca9aa7021fe37ade2c2b71e8849da87812553ad
faisalrehman <faisalfasi18@gmail.com>
2023-09-16 19:12:12 +0200

    replaced mysql to postgresql

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
+  const client = env("DATABASE_CLIENT", "postgres");
 
   const connections = {
-    mysql: {
+    postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "store"),
+        user: env("DATABASE_USERNAME", "postgresql"),
+        password: env("DATABASE_PASSWORD", "Faisal143@"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
@@ -23,6 +23,7 @@ module.exports = ({ env }) => {
             true
           ),
         },
+        schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
